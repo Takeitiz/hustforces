f23d7bc757ce1e31a7b759ee7b1685a93bc5e8ea
Takeitiz <tuandat8103@gmail.com>
2025-02-11 21:48:30 +0700

    init project

---
+++ src/main/java/com/hust/hustforces/HustforcesApplication.java
@@ -0,0 +1,13 @@
+package com.hust.hustforces;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HustforcesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HustforcesApplication.class, args);
+	}
+
+}
